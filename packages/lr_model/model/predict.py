c2357fc103598e9518cba1f432bb7dee1ad158df
Test User <testuser@gmail.com>
2020-10-11 17:35:53 +0530

    Skeleton project

---
+++ packages/lr_model/model/predict.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+from lr_model.processing.data_management import load_pipeline
+from lr_model.config import config
+
+def make_prediction():
+    pass
---
+++ packages/lr_model/tests/test_prediction.py
@@ -0,0 +1,5 @@
+from lr_model.predict import make_prediction
+from lr_model.processing.data_management import load_dataset
+
+def test_make_prediction():
+    pass
